3ac57b35c3a48a72a9170b9a694a2e78e288e30e
voltrue2 <voltrue2@gmail.com>
2014-05-30 10:53:51 +0900

    test added

--- Makefile
+++ Makefile
@@ -7,3 +7,14 @@ init:
 	@echo 'install dependencies'
 	npm install
 	@echo 'done'
+
+.PHONY: test-apple
+test-apple:
+	@echo 'test gracenode module iap apple:'
+	./node_modules/mocha/bin/mocha test/index.js -R spec -b  --path=$(path) --service=apple
+
+.PHONY: test-google
+test-google:
+	@echo 'test gracenode module iap google:'
+	./node_modules/mocha/bin/mocha test/index.js -R spec -b  --key=$(key) --path=$(path) --service=google
+
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-var gn = require('../../');
+var gn = require('gracenode');
 var assert = require('assert');
 var fs = require('fs');
+var prefix = require('./prefix');
 
 var path;
 // apple or google
@@ -9,7 +10,8 @@ var pem;
 
 describe('iap (in-app-purchase) module ->', function () {
 	
-	console.log('For this test to properly work, we need to have both Apple and Google purchase receipt files.');
+	console.log('***Notice: This test requires gracenode, gracenode-request, and gracenode-mysql installed in the same directory as this module.');
+	console.log('***Notice: For this test to properly work, we need to have both Apple and Google purchase receipt files.');
 
 	it('Can set up iap module', function (done) {
 		path = process.argv[process.argv.length - 2].replace('--path=', '');
@@ -23,16 +25,16 @@ describe('iap (in-app-purchase) module ->', function () {
 			pem = process.argv[process.argv.length - 3].replace('--key=', '');
 			// override config value for pem path for google test
 			gn.on('setup.config', function () {
-				var conf = gn.config.getOne('modules.iap');
+				var conf = gn.config.getOne('modules.gracenode-iap');
 				conf.googlePublicKeyPath = pem;
 			});
 		}
 
-		gn.setConfigPath('gracenode/test/configs/');
+		gn.setConfigPath(prefix + 'gracenode-iap/test/configs/');
 		gn.setConfigFiles(['iap.json']);
-		gn.use('request');
-		gn.use('mysql');
-		gn.use('iap');
+		gn.use('gracenode-request');
+		gn.use('gracenode-mysql');
+		gn.use('gracenode-iap');
 		gn.setup(function (error) {
 			assert.equal(error, undefined);
 			done();
